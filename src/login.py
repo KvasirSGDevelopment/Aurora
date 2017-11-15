0d926cce66a82424eecf30aa6ee8d590ec3aac1a
Luke <Lukasronsholt@gmail.com>
2017-11-15 14:10:37 +0100

    merge conflict fixed

--- src/login.py
+++ src/login.py
@@ -3,12 +3,18 @@ from src import Commands
 import os
 from discord.ext import commands
 from config.auth import User
+from config.cmd_not_found import cmd_not_found_messages
 import random
 
 __author__ = "Luke"
 __version__ = "0.0.1"
 
 modulePath = os.path.join(os.getcwd(), "src", "modules")
+
+
+def command_not_found():
+    messages = cmd_not_found_messages
+    return random.choice(messages)
 
 bot = commands.Bot(command_prefix=commands.when_mentioned_or('§'), description='AuroraV2 DevBuild PreAlpha')
 
